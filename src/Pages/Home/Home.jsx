df0d945954168073dd2bbe5c8341533994fb7881
mariaroudier <volokh.maria@gmail.com>
2023-04-30 17:13:39 +0200

    test publishing

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function Home(){
+      return (
+            <div>HELLO</div>
+      )
+}
+
+export default Home;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home/Home';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>
+    <Routes>
+      <Route path="http://localhost:3000/portfolio/" element={<Home />} />
+      {/* <Route path="/employee-list" element={<ListOfEmployees />} /> */}
+    </Routes>
+  </ BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
